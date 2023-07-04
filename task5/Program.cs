9c7b58608969c123c82c1e59e1dba89a7fd44c2c
Yuliya <jollyseed@yandex.ru>
2023-07-04 22:15:01 +0300

    add task7 in task5

--- task5/Program.cs
+++ task5/Program.cs
@@ -1,8 +1,25 @@
 ﻿int Power(int number)
 {
-return number * number;
+    return number * number;
+}
+void Task5()
+{
+    Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(Power(num));
 }
 
+void Task7()
+{
 Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(Power(num));
+num = Math.Abs(num);
+int counter = -num;
+while (counter <= num)
+{
+    Console.Write(counter + "," + " ");
+    counter++;
+}
+}
+
+Task7();
