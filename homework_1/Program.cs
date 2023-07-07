5292391169acd05055a0617fa16ede527cad64a5
Yuliya <jollyseed@yandex.ru>
2023-07-07 23:09:14 +0300

    task4 for homework1

--- homework_1/Program.cs
+++ homework_1/Program.cs
@@ -20,4 +20,29 @@
         Console.Write("Числа равны. ");
     }
 }
-    Task2();
+
+void Task4()
+{
+    //Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+    Console.Write("Введите первое число: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите второе число: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите третье число: ");
+    int c = Convert.ToInt32(Console.ReadLine());
+
+    int max = a;
+
+    if (max < b)
+    {
+        max = b;
+    }
+    if (max < c)
+    {
+        max = c;
+    }
+
+    Console.Write($"Максимальное число {max}");
+}
+
+Task4();
