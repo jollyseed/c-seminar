79a98678316a119b1bc0965b3abe19b25711f43f
Yuliya <jollyseed@yandex.ru>
2023-07-07 20:27:00 +0300

    start seminar2

---
+++ template_2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace template_2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //9. Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+        
+        
+        Random rnd = new Random();
+
+        int number = rnd.Next(10, 100);
+
+        Console.WriteLine(number);
+    }
+}
