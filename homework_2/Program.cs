9839722c82d7a8b5ece648c62955315b96ee0047
Yuliya <jollyseed@yandex.ru>
2023-07-11 22:14:34 +0300

    task 10

---
+++ homework_2/Program.cs
@@ -0,0 +1,22 @@
+﻿int Input(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Task10()
+{
+    // Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+    int a = Input("Введите трехзначное число: ");
+    if (a >= 100 && a < 1000)
+    {
+        Console.WriteLine($"Вторая цифра числа: {a / 10 % 10}");
+    }
+    else
+    {
+        Console.WriteLine($"Число не трехзначное");
+    }
+    
+    
+}
+Task10();
