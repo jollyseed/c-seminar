0b3d4f188594d190557e494250107f9ffe256839
Yuliya <jollyseed@yandex.ru>
2023-09-05 13:26:46 +0300

    task25

--- homework_4/Program.cs
+++ homework_4/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Task25()
+{
+// Задача 25: Используя определение степени числа, напишите цикл, 
+// который принимает на вход два натуральных числа (A и B) и возводит число A в степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Prompt (string message)
+{
+    System.Console.Write(message); //выводим приглашение ко вводу
+    string readInput = System.Console.ReadLine(); //вводим значение
+    int result = int.Parse(readInput); //приводим к числу
+    return result; //выводим результат
+
+}
+int Power(int powerBase, int exponent)
+{
+    int power = 1;
+    for (int i = 0; i < exponent; i++)
+    {
+        power *= powerBase;
+    }
+    return power;
+}
+bool ValidateExponent(int exponent)
+{
+    if (exponent < 0)
+    {
+        System.Console.WriteLine("Показатель не должен быть меньше нуля");
+        return false;
+    }
+    return true;
+}
+
+int powerBase = Prompt("Введите основание: ");
+int exponent = Prompt("Введите показатель: ");
+if (ValidateExponent(exponent))
+{
+    System.Console.WriteLine ($"Число {powerBase} в степени {exponent} равно {Power(powerBase, exponent)}");
+}
+}
+
+void Task27()
+{
+//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+}
+
+Task25();
