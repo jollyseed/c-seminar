94e80db6e00ab7cc5731dd74f8ab3f68b1bbd5d9
Yuliya <jollyseed@yandex.ru>
2023-09-05 13:54:42 +0300

    task27

--- homework_4/Program.cs
+++ homework_4/Program.cs
@@ -46,6 +46,26 @@ void Task27()
 //452 -> 11
 //82 -> 10
 //9012 -> 12
+int Prompt(string message)
+{
+    System.Console.Write(message); //выводим приглашение ко вводу
+    string readInput = System.Console.ReadLine(); //вводим значение
+    int result = int.Parse(readInput); //приводим к числу
+    return result; //выводим результат
 }
 
-Task25();
+int SumAllDigit(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result += number%10;
+        number /=10;
+    }
+    return result;
+}
+int number = Prompt("Введите число: ");
+System.Console.WriteLine  ($"Сумма всех числе в цифре {number} = {SumAllDigit(number)}");
+}
+
+Task27();
